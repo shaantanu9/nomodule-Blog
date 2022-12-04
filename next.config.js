52f5230948f83351ff039f6e22b07cd2dbe81af3
shaantanu9 <shantanubombatkar2@gmail.com>
2022-12-05 00:14:02 +0530

    AutoDeploy-05/12/2022, 00:14:02

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:e8c5159b1c2a959e02a7821cb27873ed25e18722e231eb0bad6a02ba05579cac
+size 128
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:02dc990f97314784430cbcd3c045af151de1de922f8c28f2e6fbb1d0e416e2ea
+size 171
